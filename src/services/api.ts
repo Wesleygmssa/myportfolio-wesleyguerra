1b99f3fb6f67b2ee0a191be4ede1c57df4107431
wesley guerra <guerra_ssa@hotmail.com>
2020-08-10 11:10:50 -0300

    update

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const URL_BACKEND_TOP = window.location.hostname.includes("localhost")
   ? "http://localhost:8080"
-  : "https://devportf.herokuapp.com/projects";
+  : "https://devportf.herokuapp.com";
 
 const api = axios.create({
   baseURL: URL_BACKEND_TOP,
