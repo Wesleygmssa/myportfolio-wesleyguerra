026694eb58fb1507f8b29cd176d4efd5ca988f19
wesley guerra <guerra_ssa@hotmail.com>
2020-08-10 17:40:44 -0300

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import Home from '../pages/home';
-import About from '../pages/about';
-import Project from '../pages/project'
+import Home from "../pages/home";
+import About from "../pages/about";
+import Project from "../pages/project";
+
+// Desafio master blaster na descrição
+const Pagina404 = () => (
+  <>
+    <a href="/">Volte para home</a>
+    <div>Página 404</div>
+  </>
+);
 
 const Routes: React.FC = () => (
-    <Switch>
-        <Route exact path="/" component={Home} />
-        <Route  path="/about" component={About} />
-        <Route  path="/project" component={Project} />
-    </Switch>
-)
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/project" component={Project} />
+    <Route component={Pagina404} />
+  </Switch>
+);
 
 export default Routes;
