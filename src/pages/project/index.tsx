eca0089b064432e79019d129539ebeb039d7580d
wesley guerra <guerra_ssa@hotmail.com>
2020-08-10 08:54:11 -0300

    update

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,89 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Title, Cards } from "./styles";
-import aluraImg from "../../assets/alura.png";
-import gobaberImg from "../../assets/gobaber.png";
-import ecoleta from "../../assets/ecoleta.png";
 import PageDefault from "../../components/PageDefault";
+import api from "../../services/api";
+
+interface Repository {
+  id: string;
+  title: string;
+  description: string;
+  visita: string;
+  github: string;
+  img: string;
+}
 
 const Project: React.FC = () => {
+  const [projects, setProjects] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    api.get(`projects/`).then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
   return (
     <>
       <PageDefault>
         <Title>Explore repositórios no Github.</Title>
         <Cards>
-          <div className="card">
-            <div className="cardimg">
-              <img src={aluraImg} alt="" />
+          {projects.map((project) => (
+            <div key={project.id} className="card">
+              <div className="cardimg">
+                <img src={project.img} alt="" />
+              </div>
+              <div className="card-content">
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+              </div>
+              <div className="grupButton">
+                <a className="ButtonLink" href={project.visita} target="_blank">
+                  Visitar
+                </a>
+                <a className="ButtonLink" href={project.github} target="_blank">
+                  GitHub
+                </a>
+              </div>
             </div>
-            <div className="card-content">
-              <h3>AluraFlix</h3>
-              <p>
-                Projeto feito durante a Imersão React da Alura, Projeto baseado
-                na netflix
-              </p>
-            </div>
-            <div className="grupButton">
-              <a
-                className="ButtonLink"
-                href="https://aluraflix-gules.vercel.app/"
-                target="_blank"
-              >
-                Visitar
-              </a>
-              <a
-                className="ButtonLink"
-                href="https://github.com/Wesleygmssa/aluraflix"
-                target="_blank"
-              >
-                GitHub
-              </a>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="cardimg">
-              <img src={gobaberImg} alt="" />
-            </div>
-            <div className="card-content">
-              <h3>Gobarber</h3>
-              <p>
-                Curso de rocketseat usando como tecnologias mais avançadas para
-                a construção do sistema de agendamento de barbearia.
-              </p>
-            </div>
-            <div className="grupButton">
-              <a
-                className="ButtonLink"
-                href="https://github.com/Wesleygmssa/gobaber-web"
-                target="_blank"
-              >
-                GitHub
-              </a>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="cardimg">
-              <img src={ecoleta} alt="" />
-            </div>
-            <div className="card-content">
-              <h3>Ecoleta</h3>
-              <p>
-                Este é um repositório da Semana do Próximo Nível # 1 | Curso
-                inicial. O objetivo deste repositório é analisar e comparar a
-                evolução do meu aprendizado.
-              </p>
-            </div>
-            <div className="grupButton">
-              <a
-                className="ButtonLink"
-                href="https://github.com/Wesleygmssa/nlw1-booster"
-                target="_blank"
-              >
-                GitHub
-              </a>
-            </div>
-          </div>
+          ))}
         </Cards>
       </PageDefault>
     </>
--- src/pages/project/styles.ts
+++ src/pages/project/styles.ts
@@ -1,26 +1,26 @@
-import styled from 'styled-components'
-import { shade } from 'polished';
+import styled from "styled-components";
+import { shade } from "polished";
 
 export const Title = styled.h1`
-color: #3a3a3a;
-max-width: 350px;
-margin-bottom: 40px;
-
-`
+  color: #3a3a3a;
+  max-width: 350px;
+  margin-bottom: 40px;
+`;
 export const Cards = styled.div`
- box-sizing: border-box;
- display: flex;
- flex-wrap: wrap;
- height: 100vh;
- justify-content: center;
+  box-sizing: border-box;
+  display: flex;
+  flex-wrap: wrap;
+  height: 100vh;
+  justify-content: center;
 
- 
-
-.card{
-    & + .card{
-       
+  .card {
+    .disabled {
+      opacity: 0.5;
     }
-   
+
+    & + .card {
+    }
+
     width: 300px;
     max-height: 420px;
     border: 1px solid #ddd;
@@ -30,77 +30,73 @@ export const Cards = styled.div`
     margin-right: 5px;
     border-radius: 8px;
 
-    -webkit-box-shadow: 3px 9px 29px -13px rgba(0,0,0,0.52);
-    -moz-box-shadow: 3px 9px 29px -13px rgba(0,0,0,0.52);
-    box-shadow: 3px 9px 29px -13px rgba(0,0,0,0.52);
-    
-    .cardimg{
-        position: relative;
-        border-radius:8px;
-      
-    
-      
-        img{
-            border-radius:8px;
-            width: 100%;
-            height: 150px;
-          
-        }
+    -webkit-box-shadow: 3px 9px 29px -13px rgba(0, 0, 0, 0.52);
+    -moz-box-shadow: 3px 9px 29px -13px rgba(0, 0, 0, 0.52);
+    box-shadow: 3px 9px 29px -13px rgba(0, 0, 0, 0.52);
+
+    .cardimg {
+      position: relative;
+      border-radius: 8px;
+
+      img {
+        border-radius: 8px;
+        width: 100%;
+        height: 150px;
+      }
     }
 
-    .card-content{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 16px 8px;
-        flex-direction: column;
+    .card-content {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 16px 8px;
+      flex-direction: column;
 
-        h3{
-            width: 100%;
-            text-align: center;
-            padding: 16px 0px;
-            font-size: 24px;
-        }
+      h3 {
+        width: 100%;
+        text-align: center;
+        padding: 16px 0px;
+        font-size: 24px;
+      }
 
-        p{
-            width: 100%;
-            padding: 0px 8px;
-            text-align: justify;
-            height: 100px;
-            color: #616161;
-            font-weight: 700;
-        }
+      p {
+        width: 100%;
+        padding: 0px 8px;
+        text-align: justify;
+        height: 100px;
+        color: #616161;
+        font-weight: 700;
+      }
     }
 
-    .grupButton{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-          
-        a{
-            flex:1;
-            margin-left: 5px;
-            margin-right: 5px;
-            text-align: center;
-            color: var(--white);
-            border: 1px solid #bfbfbf;
-            box-sizing: border-box;
-            cursor: pointer;
-            padding: 16px 16px;
-            font-style: normal;
-            font-weight: bold;
-            font-size: 16px;
-            outline: none;
-            border-radius: 5px;
-            text-decoration: none;
-            display: inline-block;
-            transition: opacity .3s;
-&:hover,
-&:focus {
- opacity: .5;
-}
+    .grupButton {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      a {
+        flex: 1;
+        margin-left: 5px;
+        margin-right: 5px;
+        text-align: center;
+        color: var(--white);
+        border: 1px solid #bfbfbf;
+        box-sizing: border-box;
+        cursor: pointer;
+        padding: 16px 16px;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 16px;
+        outline: none;
+        border-radius: 5px;
+        text-decoration: none;
+        display: inline-block;
+        transition: opacity 0.3s;
+        &:hover,
+        &:focus {
+          opacity: 0.5;
         }
+      }
     }
-}
-         
+  }
 `;
