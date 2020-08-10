f4cb8fca081a000aaddeed898e9c9bc8bfdb50ca
wesley guerra <guerra_ssa@hotmail.com>
2020-08-10 10:54:36 -0300

    update

--- src/components/PageDefault/index.tsx
+++ src/components/PageDefault/index.tsx
@@ -4,14 +4,14 @@ import Footer from "../Footer";
 import Header from "../Header";
 import Zap from "../Zap";
 
-const Main = styled.main`
-  background-color: var(--black);
-  color: var(--white);
-  flex: 1;
-  padding-top: 50px;
-  padding-left: 5%;
-  padding-right: 5%;
-`;
+// const Main = styled.main`
+//   background-color: var(--black);
+//   color: var(--white);
+//   flex: 1;
+//   padding-top: 50px;
+//   padding-left: 5%;
+//   padding-right: 5%;
+// `;
 
 const PageDefault: React.FC = ({ children }) => {
   return (
