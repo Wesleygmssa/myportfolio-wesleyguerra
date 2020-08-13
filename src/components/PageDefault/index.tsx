6eb81cf78e7a11078b16cfac69ca3a0b6c35a204
wesley guerra <guerra_ssa@hotmail.com>
2020-08-13 08:38:10 -0300

    update

--- src/components/PageDefault/index.tsx
+++ src/components/PageDefault/index.tsx
@@ -18,12 +18,20 @@ const apperFromCenter = keyframes`
 const AnimationContainer = styled.div`
   animation: ${apperFromCenter} 1s;
 `;
+const Text = styled.p`
+  text-align: center;
+  font-size: 24px;
+  color: #fff;
+  opacity: 0.8;
+  margin-top: 16px;
+`;
 
 const PageDefault: React.FC = ({ children }) => {
   return (
     <>
       <Header />
       <AnimationContainer>{children}</AnimationContainer>
+      <Text>Ferramentas utlizadas para construção desse Portfólio</Text>
       <Footer />
       <Zap />
     </>
